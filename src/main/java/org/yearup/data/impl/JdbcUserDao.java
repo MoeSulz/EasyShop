5809f726b7c840a43589eafcb7379d733a38df2d
MoeSulz <msuleiman@appdev.yearup.org>
2023-06-30 09:21:55 -0400

    Touched up methods

--- src/main/java/org/yearup/data/impl/JdbcUserDao.java
+++ src/main/java/org/yearup/data/impl/JdbcUserDao.java
@@ -6,7 +6,7 @@ import org.yearup.data.UserDao;
 import org.yearup.models.User;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,17 +23,74 @@ public class JdbcUserDao implements UserDao {
         String query = "SELECT * FROM users;";
         List<User> users = new ArrayList<>();
 
-        try(Connection connection = dataSource.getConnection())
+        try(Connection connection = dataSource.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            ResultSet resultSet = preparedStatement.executeQuery()) {
+
+            while (resultSet.next()){
+                int userID = resultSet.getInt(1);
+                String username = resultSet.getString(2);
+                String password = resultSet.getString(3);
+                String authority = resultSet.getString(4);
+
+                users.add(new User(userID, username, password, authority));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return users;
     }
 
     @Override
     public User getUserById(int userId) {
-        return null;
+        String query = "SELECT * FROM users WHERE user_id = ?;";
+        User user = null;
+
+        try (Connection connection = dataSource.getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement(query)){
+
+            preparedStatement.setInt(1, userId);
+
+            try (ResultSet resultSet = preparedStatement.executeQuery()){
+                while(resultSet.next()){
+                    int userID = resultSet.getInt(1);
+                    String username = resultSet.getString(2);
+                    String password = resultSet.getString(3);
+                    String authority = resultSet.getString(4);
+
+                    user = new User(userID, username, password, authority);
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return user;
     }
 
     @Override
     public User getByUserName(String username) {
-        return null;
+        String query = "SELECT * FROM users WHERE username = ?;";
+        User user = null;
+
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query)){
+
+            preparedStatement.setString(1, username);
+
+            try (ResultSet resultSet = preparedStatement.executeQuery()){
+                while(resultSet.next()){
+                    int userID = resultSet.getInt(1);
+                    String usernameFromDB = resultSet.getString(2);
+                    String password = resultSet.getString(3);
+                    String authority = resultSet.getString(4);
+
+                    user = new User(userID, usernameFromDB, password, authority);
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return user;
     }
 
     @Override
@@ -43,7 +100,25 @@ public class JdbcUserDao implements UserDao {
 
     @Override
     public User create(User user) {
-        return null;
+        String query = "INSERT INTO users (username, hashed_password, role) values (?,?,?);";
+
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS)) {
+
+            preparedStatement.setString(1, user.getUsername());
+            preparedStatement.setString(2, user.getPassword());
+            preparedStatement.setString(3, user.getRole());
+
+            try(ResultSet keys = preparedStatement.getGeneratedKeys()){
+                if (keys.next()){
+                    int generatedID = keys.getInt(1);
+                    user.setId(generatedID);
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return user;
     }
 
     @Override
